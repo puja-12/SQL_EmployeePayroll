8e5d1679afebeb5e11d74636aa1513a4ad90c14b
puja <pujarana1796@gmail.com>
2022-06-03 13:15:11 +0530

    To Create Payroll Data

--- Payroll_serviceSQL.sql
+++ Payroll_serviceSQL.sql
@@ -4,4 +4,9 @@ use payroll_service
 
 UC2: To create Table
 create table employee_payroll
-(id INT PRIMARY KEY IDENTITY (1,1),Name varchar(20), Salary int,Start_Date DATE) 
+(id INT identity(1,1)PRIMARY KEY,Name varchar(20), Salary int,Start_Date DATE) 
+
+UC3: to insert data into table
+insert into employee_payroll values
+('Pooja',20000,'2022-02-10'),('Rahul',30000,'2022-04-12'),('Vivek',40000,'2019-05-15');
+
