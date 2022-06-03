763bcee9cf63cc42a5aed679de3f9b5a0bf950ac
puja <pujarana1796@gmail.com>
2022-06-03 12:58:16 +0530

    Create table

--- Payroll_serviceSQL.sql
+++ Payroll_serviceSQL.sql
@@ -1,7 +1,7 @@
-UC1
+UC1: To create database
 CREATE DATABASE payroll_service
 use payroll_service
 
-UC2
+UC2: To create Table
 create table employee_payroll
 (id INT PRIMARY KEY IDENTITY (1,1),Name varchar(20), Salary int,Start_Date DATE) 
